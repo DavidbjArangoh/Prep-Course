cc3621e3582b22d87ec47daa67a4a3370f8f4ac5
DavidbjArangoh <94635996+DavidbjArangoh@users.noreply.github.com>
2022-11-25 17:25:44 -0500

    homework V y VI

--- 07-JS-VI/homework/homework.js
+++ 07-JS-VI/homework/homework.js
@@ -1,20 +1,30 @@
 // Do not change any of the function names
 
+const readingTime = require("henry-reading-time/lib/reading-time");
+
 function mayuscula(nombre) {
   //La función recibe un nombre y debe devolver el mismo que recibe pero con su primer letra en mayúscula
   //ej: Recibe "mario" ----> Devuelve "Mario"
   //Tu código:
+  var I = nombre[0].toUpperCase();
+  for (var i = 1; i < nombre.length; i++){
+    I = I + nombre[i];
+  }
+
+  return I;
 }
 
 function invocarCallback(cb) {
   // Invoca al callback `cb`
   //Tu código:
+  cb();
 }
 
 function operacionMatematica(n1, n2, cb) {
   //Vamos a recibir una función que realiza una operación matemática como callback junto con dos números.
   //Devolver el callback pasándole como argumentos los números recibidos.
   //Tu código:
+  return cb(n1,n2);
 }
 
 function sumarArray(numeros, cb) {
@@ -22,12 +32,15 @@ function sumarArray(numeros, cb) {
   // Pasa el resultado a `cb`
   // No es necesario devolver nada
   //Tu código:
+  
+  cb(numeros.reduce(function(x, y) {return x + y;}));
 }
 
 function forEach(array, cb) {
   // Itera sobre la matriz "array" y pasa los valores al callback uno por uno
   // Pista: Estarás invocando a `cb` varias veces (una por cada valor en la matriz)
   //Tu código:
+  array.forEach(cb);
 }
 
 function map(array, cb) {
@@ -35,12 +48,23 @@ function map(array, cb) {
   // Itera sobre cada valor en "array", pásalo a `cb` y luego ubicar el valor devuelto por `cb` en un nuevo array
   // El nuevo array debe tener la misma longitud que el array del argumento
   //Tu código:
+  var nArray = array.map(cb);
+  return nArray;
 }
 
 function filter(array) {
   //Filtrar todos los elementos del array que comiencen con la letra "a".
   //Devolver un nuevo array con los elementos que cumplen la condición
   //Tu código:
+  var nArray = new Array();
+  function comienzaA(palabra){
+    if (palabra[0] === 'a'){
+      nArray.push(palabra);
+    } 
+  }
+  array.forEach(comienzaA); 
+  return nArray;
+
 }
 
 // No modificar nada debajo de esta línea
